33bb9c320d347cc903bd9dedc58bbfc244074b08
Sazzad Hossain <tanimsazzad@gmail.com>
2023-10-29 15:51:32 +0600

    log before returning

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -4,8 +4,8 @@ export async function POST(req: Request) {
   const userMessageFromClient = await req.json();
 
   if (!userMessageFromClient) {
-    return Response.json({ error: "empty user message" });
     console.dir("empty user message");
+    return Response.json({ error: "empty user message" });
   }
 
   try {
