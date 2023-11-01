542c79043cd80f2ef51db4f9abe5fdf987dd059f
Sazzad Hossain <tanimsazzad@gmail.com>
2023-11-01 19:14:00 +0600

    added prettier

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,19 +1,19 @@
-import { chatModel, llm } from "@/lib/openai";
+import { chatModel, llm } from '@/lib/openai'
 
 export async function POST(req: Request) {
-  const userMessageFromClient = await req.json();
+  const userMessageFromClient = await req.json()
 
   if (!userMessageFromClient) {
-    console.dir("empty user message");
-    return Response.json({ error: "empty user message" });
+    console.dir('empty user message')
+    return Response.json({ error: 'empty user message' })
   }
 
   try {
-    const llmResult = await llm.predict(userMessageFromClient);
-    const chatModelResult = await chatModel.predict(userMessageFromClient);
-    return Response.json({ llmResult, chatModelResult });
+    const llmResult = await llm.predict(userMessageFromClient)
+    const chatModelResult = await chatModel.predict(userMessageFromClient)
+    return Response.json({ llmResult, chatModelResult })
   } catch (error) {
-    console.dir({ error });
-    return Response.json({ error });
+    console.dir({ error })
+    return Response.json({ error })
   }
 }
--- lib/openai.ts
+++ lib/openai.ts
@@ -1,8 +1,7 @@
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { OpenAI } from "langchain/llms/openai";
+import { ChatOpenAI } from 'langchain/chat_models/openai'
+import { OpenAI } from 'langchain/llms/openai'
 
-const llm = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY });
-const chatModel = new ChatOpenAI();
+const llm = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY })
+const chatModel = new ChatOpenAI()
 
-export { chatModel, llm };
-
+export { chatModel, llm }
